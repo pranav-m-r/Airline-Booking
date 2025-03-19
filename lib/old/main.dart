18b945b90439c180e320e6993f2e1cb7e16cc546
Pranav M R <148135964+pranav-m-r@users.noreply.github.com>
2025-03-20 00:21:04 +0530

    Project refactoring

---
+++ lib/old/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Airline Booking System',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:airlinebooking/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(SkyLinerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
